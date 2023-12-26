5dbf1e4886906a3a6f7facd14905f09e44411b06
motivational-dragon <116593274+motivational-dragon@users.noreply.github.com>
2023-12-26 21:57:03 +0100

    Revert attempt to implement a new recipe for the cauldron.

---
+++ Common/src/main/java/mod/motivationaldragon/potionblender/recipes/PotionBlenderSpecialRecipeSerializer.java
@@ -0,0 +1,23 @@
+package mod.motivationaldragon.potionblender.recipes;
+
+import mod.motivationaldragon.potionblender.Constants;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.SimpleCraftingRecipeSerializer;
+
+
+import java.util.function.BiConsumer;
+
+public class PotionBlenderSpecialRecipeSerializer {
+
+    public static final SimpleCraftingRecipeSerializer<CombinedTippedArrowRecipe> COMBINED_TIPPED_ARROW = new SimpleCraftingRecipeSerializer<>(CombinedTippedArrowRecipe::new);
+
+
+
+    public static void register(BiConsumer<RecipeSerializer<?>, ResourceLocation> r){
+        r.accept(COMBINED_TIPPED_ARROW ,new ResourceLocation(Constants.MOD_ID,"tipped_combined_arrow"));
+        Constants.LOG.debug("Loaded recipe");
+    }
+
+
+}
--- Fabric/src/main/java/mod/motivationaldragon/potionblender/FabricPotionBlender.java
+++ Fabric/src/main/java/mod/motivationaldragon/potionblender/FabricPotionBlender.java
@@ -4,14 +4,13 @@ import mod.motivationaldragon.potionblender.block.PotionBlenderBlock;
 import mod.motivationaldragon.potionblender.blockentity.FabricBlockEntities;
 import mod.motivationaldragon.potionblender.event.OnUseBlockFabric;
 import mod.motivationaldragon.potionblender.item.ModItem;
-import mod.motivationaldragon.potionblender.recipes.PotionBlenderRecipe;
+import mod.motivationaldragon.potionblender.recipes.PotionBlenderSpecialRecipeSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.crafting.RecipeType;
 
 import java.util.function.BiConsumer;
 
@@ -24,8 +23,7 @@ public class FabricPotionBlender implements ModInitializer {
 
 		PotionBlenderBlock.registerBlock(bind(BuiltInRegistries.BLOCK));
 		PotionBlenderBlock.registerBlockItem(bind(BuiltInRegistries.ITEM));
-		PotionBlenderRecipe.registerRecipeSerializer(bind(BuiltInRegistries.RECIPE_SERIALIZER));
-		PotionBlenderRecipe.registerRecipeType(bind(BuiltInRegistries.RECIPE_TYPE));
+		PotionBlenderSpecialRecipeSerializer.register(bind(BuiltInRegistries.RECIPE_SERIALIZER));
 
 		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FUNCTIONAL_BLOCKS).register(entries -> ModItem.registerFunctionalBlocksItems(entries::accept));
 
