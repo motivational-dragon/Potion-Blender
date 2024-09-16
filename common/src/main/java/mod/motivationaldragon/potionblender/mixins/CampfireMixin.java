0f7c433319f7e82f03304f01ca97cf87effd93d4
happy44300 <116593274+motivational-dragon@users.noreply.github.com>
2024-09-16 11:27:52 +0200

    Correct cauldron appearing for 1 frame and then disappearing when placed on campfire.

--- common/src/main/java/mod/motivationaldragon/potionblender/mixins/CampfireMixin.java
+++ common/src/main/java/mod/motivationaldragon/potionblender/mixins/CampfireMixin.java
@@ -3,9 +3,10 @@ package mod.motivationaldragon.potionblender.mixins;
 import mod.motivationaldragon.potionblender.block.BrewingCauldron;
 import mod.motivationaldragon.potionblender.block.PotionBlenderBlock;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -29,12 +30,12 @@ public abstract class CampfireMixin {
 	@Shadow @Final public static DirectionProperty FACING;
 
 	@Inject(method = "useItemOn", at = @At("HEAD"), cancellable = true)
-	public void useItemOn(ItemStack ignored,BlockState campfireBlockstate, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<InteractionResult> cir) {
+	public void useItemOn(ItemStack ignored,BlockState campfireBlockstate, Level level, BlockPos blockPos, Player player, InteractionHand interactionHand, BlockHitResult blockHitResult, CallbackInfoReturnable<ItemInteractionResult> cir) {
 
-		if(level.isClientSide() || player.isSpectator()) {return;}
+		if(player.isSpectator()) {return;}
 
 		ItemStack itemInHand = player.getItemInHand(interactionHand);
-		if(itemInHand.is(Items.CAULDRON) && blockHitResult.getDirection() == Direction.UP){
+		if(itemInHand.is(Items.CAULDRON)){
 			if(!player.isCreative()) {
 				itemInHand.shrink(1);}
 
@@ -44,9 +45,10 @@ public abstract class CampfireMixin {
 			cauldronBlockState = cauldronBlockState.setValue(BrewingCauldron.IS_SOULFIRE, campfireBlockstate.getBlock().equals(Blocks.SOUL_CAMPFIRE));
 			cauldronBlockState = cauldronBlockState.setValue(BrewingCauldron.FACING, campfireBlockstate.getValue(FACING));
 
+			level.playSound(null, blockPos, SoundEvents.WOOD_PLACE, SoundSource.BLOCKS, 1F, 1F);
 
 			Block.updateOrDestroy(null, cauldronBlockState, level, blockPos,0);
-			cir.setReturnValue(InteractionResult.CONSUME);
+			cir.setReturnValue(ItemInteractionResult.CONSUME);
 		}
 	}
 }
